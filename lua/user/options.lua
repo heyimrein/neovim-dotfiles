988bfda4a14a913ad12f5536c44d784dc4c890dd
reinlmao <rekreingd@gmail.com>
2023-08-07 14:28:22 -0500

    Update options.lua
    Add 'list' option

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,13 +1,17 @@
 local options = {
-  wrap = false,
-  number = true,
-  numberwidth = 4,
-  smartindent = true,
-  tabstop = 2,
-  shiftwidth = 2,
-  showtabline = 2,
+	wrap = false,
+	number = true,
+	numberwidth = 4,
+	smartindent = true,
+	tabstop = 2,
+	shiftwidth = 2,
+	showtabline = 2,
+	scrolloff = 8,
+	list = true,
 }
 
+vim.opt.listchars = { tab = "⁖⁖", eol = "◃" }
+
 for k, v in pairs(options) do
-  vim.o[k] = v
+	vim.o[k] = v
 end
