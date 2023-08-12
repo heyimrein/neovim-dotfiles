18d4382bb9cd85d79cc11669f32383600e26d3c2
reinlmao <rekreingd@gmail.com>
2023-08-12 15:41:30 -0500

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,10 @@
--- require "nvim-treesitter".setup {}
+require "nvim-treesitter".setup {}
+require "nvim-treesitter.configs".setup {
+	autotag = {
+		enable = true,
+		enable_rename = true,
+		enable_close = true,
+		enable_close_on_slash = true,
+		filetypes = { "html", "tsx", "typescriptreact", "jsx", "javascriptreact" },
+	}
+}
