8927c496fe17d5520c7b32283fba4107d0852930
reinlmao <rekreingd@gmail.com>
2023-08-18 10:42:01 -0500

    Change listchars

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,14 +4,14 @@ local options = {
 	numberwidth = 4,
 	smartindent = true,
 	tabstop = 2,
-	showtabstop = 4,
 	softtabstop = 2,
 	shiftwidth = 2,
 	scrolloff = 8,
 	list = true,
+	hlsearch = false,
 }
 
-vim.opt.listchars = { tab = "⁖⁖", eol = "◃" }
+vim.opt.listchars = { tab = "⁖⁖", lead = "⁖" }
 
 for k, v in pairs(options) do
 	vim.o[k] = v
