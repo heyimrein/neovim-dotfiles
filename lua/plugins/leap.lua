6a4d9b94ab56938f152e1a8afcc895db35de364d
reinlmao <rekreingd@gmail.com>
2023-09-08 15:42:29 -0500

    Add extra mappings for Mac

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,3 +2,5 @@ local map = vim.keymap.set
 
 map("n", "<C-s>", "<Plug>(leap-forward-to)")
 map("n", "<C-d>", "<Plug>(leap-backward-to)")
+map("n", "<Leader>s", "<Plug>(leap-forward-to)")
+map("n", "<Leader>d", "<Plug>(leap-backward-to)")
