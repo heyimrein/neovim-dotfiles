96dd96313b72eaa4cef1e461e440f7032c78d8bf
reinlmao <rekreingd@gmail.com>
2023-09-07 20:49:43 -0500

    Ensure LSP's are installed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,7 @@
 require "mason".setup()
-require "mason-lspconfig".setup()
+require "mason-lspconfig".setup {
+	ensure_installed = { "lua_ls", "rust_analyzer", "html", "cssls", "tsserver", "pyright" }
+}
 
 local lsps = {
 	"lua_ls",
